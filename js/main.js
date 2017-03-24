64e2305a56d0eca8053a932817086523243347c0
Marco Ledezma <marcoledezmacordero09@gmail.com>
2017-03-23 18:03:17 -0600

    Render of the table finished

--- js/main.js
+++ js/main.js
@@ -13,7 +13,6 @@
     tHeader.appendChild(firstTr);
 
     function addColumn() {
-        tbody.push([tbody.length+1])
         thead.push(headers[thead.length]);
         _renderTable('head');
     }
@@ -23,39 +22,68 @@
         _renderTable('body');
     }
 
-    function _renderHead(){
+    function _renderHead() {
         tHeader.innerHTML = '';
         var tr = document.createElement('tr');
         var th = document.createElement('th');
         tr.appendChild(th);
-        for (var i = 0; i < thead.length; i++) {
+
+        // Render the body
+        _renderTable('body');
+
+        for(var i = 0; i < thead.length; i++) {
             var th = document.createElement('th');
             th.textContent = thead[i];
             tr.appendChild(th);
             tHeader.appendChild(tr);
-            console.log(thead[i]);
         };
     }
 
-    function _renderBody(){
+    function _renderBody() {
+        var mainId = 0;
+        var secondId = 0;
         tBody.innerHTML = '';
         for(var i = 0; i < tbody.length; i++) {
             var tr = document.createElement('tr');
-            console.log(tbody[i][0]);
-            var td = document.createElement('td');
-            td.textContent = tbody[i][0]
-            tr.appendChild(td);
+            var firstTd = document.createElement('td');
+            firstTd.textContent = tbody[i][0];
+            firstTd.id = 'row' + mainId;
+            mainId++;
+            tr.appendChild(firstTd);
+
+            // Row validation
+            _rowValidation();
+            
+            for(var j = 0; j < thead.length; j++) {
+                var td = document.createElement('td');
+                td.textContent = tbody[i][j+1];
+                td.id = secondId;
+                secondId++;
+                tr.appendChild(td);
+            }     
+
             tBody.appendChild(tr);
         }
     }
 
-    function _renderTable(render){
+    function _renderTable(render) {
         if(render === 'head') {
             _renderHead();
         }
 
         if(render === 'body') {
             _renderBody();
+        }
+    }
+    
+    // Validates if a row exists and then appends a 'td'
+    function _rowValidation() {
+        if (tbody.length !== 0) {
+            for(var i = 0; i < tbody.length; i++) {
+                for(var j = 0; j < thead.length; j++) {
+                    tbody[i].push('test');
+                }
+            }
         }
     }
 
